dbdbe2f0b61a3282bd010076d99e0a176967900b
nhat416 <nhatt416@gmail.com>
2021-10-22 15:30:32 -0400

    test breaking pytest

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,5 +1,11 @@
 from hello.hello_world import say_hello
 
+
 def test_say_hello():
     msg = say_hello('Hello World!')
+    assert msg == 'Hello World!'
+
+
+def test_fail_test():
+    msg = say_hello('hello')
     assert msg == 'Hello World!'
